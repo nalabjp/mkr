f7e445703d716f90e1576105b390bf8fbded4c08
nalabjp <nalabjp@gmail.com>
2017-05-26 03:09:10 +0900

    Create webhook entrypoint

---
+++ app.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+require_relative 'lib/mkr'
+
+class App < Sinatra::Base
+  include Mkr::Logging
+
+  ACTIONS = {
+    entered: :punch_in,
+    exited: :punch_out
+  }.freeze
+
+  post '/' do
+    params = JSON.parse(request.body.read)
+    action = ACTION.fetch(params['action'].to_sym)
+    if action
+      info("Process `#{params['action']}` acction")
+      Mkr.run(action)
+      success("Process `#{params['action']}` acction")
+      Mkr.run(action)
+      # TODO: notify success?
+    else
+      failure("Not found action: `#{params['action']}`")
+      # TODO: notify failure?
+  end
+end
---
+++ config.ru
@@ -0,0 +1,10 @@
+ROOT_DIR = File.dirname(__FILE__)
+$LOAD_PATH.unshift(ROOT_DIR) unless $LOAD_PATH.include?(ROOT_DIR)
+
+ENV['RACK_ENV'] ||= 'development'
+
+require 'bundler'
+Bundler.setup(:default, ENV['RACK_ENV'])
+
+require 'app'
+run App
