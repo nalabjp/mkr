628d49c53348a8dc61a12d447f46e37b20770aac
nalabjp <nalabjp@gmail.com>
2017-05-26 03:01:33 +0900

    Draft `Mkr`

---
+++ lib/mkr.rb
@@ -0,0 +1,14 @@
+require 'capybara/poltergeist'
+require_relative 'mkr/logger'
+require_relative 'mkr/logging'
+require_relative 'mkr/user'
+require_relative 'mkr/job'
+
+module Mkr
+  class << self
+    def run(action)
+      user = User.from_env
+      Job.new(user, action).execute
+    end
+  end
+end
