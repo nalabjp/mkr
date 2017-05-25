6e3075df7457e16b2e68fda43dbfaba755acdf85
nalabjp <nalabjp@gmail.com>
2017-05-26 03:00:00 +0900

    Draft `Logger`

---
+++ lib/mkr/logging.rb
@@ -0,0 +1,14 @@
+module Mkr
+  module Logging
+    def self.included(klass)
+      klass.class_eval do
+        extend Forwardable
+        def_delegators :logger, *Logger.public_instance_methods(false)
+      end
+    end
+
+    def logger
+      @logger ||= Logger.instance
+    end
+  end
+end
